561e0a531cdf459507415a431745070865d5f32d
yasirghouri <yasir.ag7@gmail.com>
2023-02-19 13:31:16 +0500

    setup chakra UI

---
+++ src/chakra/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF3C00",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+});
